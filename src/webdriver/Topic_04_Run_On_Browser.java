d6f2f199a5071085d6b3a7fd4d695e5b8e48acdf
MinhTN <128308496+mjnhk0n@users.noreply.github.com>
2023-04-06 17:05:18 +0700

    Topic 05 web browser

--- src/webdriver/Topic_04_Run_On_Browser.java
+++ src/webdriver/Topic_04_Run_On_Browser.java
@@ -1,14 +1,8 @@
 package webdriver;
 
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class Topic_04_Run_On_Browser {
