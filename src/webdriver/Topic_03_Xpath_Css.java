f5ba267558550e297329158320ede28d2f3bd000
MinhTN <128308496+mjnhk0n@users.noreply.github.com>
2023-04-06 16:39:45 +0700

    Run on many browsers

--- src/webdriver/Topic_03_Xpath_Css.java
+++ src/webdriver/Topic_03_Xpath_Css.java
@@ -5,14 +5,12 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.server.handler.SendKeys;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Topic_03_Xpath_Part1 {
+public class Topic_03_Xpath_Css {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
 	String osName = System.getProperty("os.name");
