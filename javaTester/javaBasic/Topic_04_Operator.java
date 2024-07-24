b363611f45960130ac5fb685106a13094f687315
minhtn.pegaxy <143593932+minhtn-pegaxy@users.noreply.github.com>
2024-07-24 17:51:39 +0700

    Topic 04 update

--- javaTester/javaBasic/Topic_04_Operator.java
+++ javaTester/javaBasic/Topic_04_Operator.java
@@ -21,11 +21,40 @@ public class Topic_04_Operator {
 		
 	int a,b = 10, c = 5;
 	
-	a = b + c;
-	
+	a = b+c;
 	System.out.println("Cong = " + a);
+	
+	a = b-c;
+	System.out.println("Tru = " + a);
+	
+	a = b*c;
+	System.out.println("Nhan = " + a);
+	
+	a = b/c;
+	System.out.println("Chia lay so nguyen = " + a);
+	
+	a = b%c;
+	System.out.println("Chia lay so du = " + a);
+	
+	b++;
+	System.out.println("Tang len 1 = " + b);
+	
+	b--;
+	System.out.println("Giam xuong 1 = " + b);
 	
 	}
 	
+	@Test
+	public void TC_03(){
+		
+	// ++ in prefix and postfix
+		int firstVariable = 5, secondVariable = 7;
+		int result = firstVariable++ + ++secondVariable - 8;
+		
+		System.out.println("First variable = " + firstVariable);
+		System.out.println("Second variable = " + secondVariable);
+		System.out.println("Third variable = " + result);
+		
+	}
 
 }
