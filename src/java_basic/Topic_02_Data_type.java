c32231fa377b03fcac562cf49ada0c1a95ceb679
minhtn.pegaxy <143593932+minhtn-pegaxy@users.noreply.github.com>
2024-08-27 16:08:10 +0700

    Refactor Topic 02 Data_type

---
+++ src/java_basic/Topic_02_Data_type.java
@@ -0,0 +1,41 @@
+package java_basic;
+
+public class Topic_02_Data_type {
+	// Global variable
+	static int number;
+	
+	String address = "Sai Gon";
+	
+	public static void main (String[] args) {
+		// Local variable
+		int studentNumber = 0;
+		System.out.println(number);
+		
+		Topic_02_Data_type topic = new Topic_02_Data_type();
+		
+		System.out.println(topic.address);
+		
+		System.out.println(studentNumber);
+	}
+	
+	// Primitive type/ value type: Nguyên thủy
+	byte bNumber;
+	
+	short sNumber;
+	
+	int iNumber;
+	
+	long lNumber;
+	
+	float fNumber;
+	
+	double dNumber;
+	
+	char cChar;
+	
+	boolean bStattus;
+	
+	// Reference type: Tham chiếu
+	
+
+}
