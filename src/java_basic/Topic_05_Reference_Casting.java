efff97aa0e884f0eda6de8e9993d2625f4e8166c
minhtn.pegaxy <143593932+minhtn-pegaxy@users.noreply.github.com>
2024-09-12 11:08:54 +0700

    Topic 05 ref casting

--- src/java_basic/Topic_05_Reference_Casting.java
+++ src/java_basic/Topic_05_Reference_Casting.java
@@ -1,8 +1,47 @@
-package java_basic;
+package java_basic;import java.awt.SecondaryLoop;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 
 public class Topic_05_Reference_Casting {
-	public static void main(String[] args) {
-		protected String studentName;
+	
+	protected String studentName;
 		
+	public String getStudentName() {
+		return studentName;
+	}
+	
+	public void setStudentName (String studentName) {
+		this.studentName = studentName;
+	}
+	
+	public void showStudentInfor() {
+		System.out.println("Student Name = " + studentName);
+	}
+	public static void main (String[] agrs) {
+		Topic_05_Reference_Casting firstStudent = new Topic_05_Reference_Casting();
+		Topic_05_Reference_Casting secondStudent = new Topic_05_Reference_Casting();
+		
+		firstStudent.setStudentName("Student A");
+		secondStudent.setStudentName("Student B");
+		
+		firstStudent.showStudentInfor();
+		secondStudent.showStudentInfor();
+		
+		// Cast
+		firstStudent = secondStudent;
+		
+		firstStudent.showStudentInfor();
+		secondStudent.showStudentInfor();
+		
+		secondStudent.setStudentName("Student C");
+		
+		firstStudent.showStudentInfor();
+		secondStudent.showStudentInfor();
+		
+		WebDriver driver = null;
+		
+		JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
 	}
 }
+
