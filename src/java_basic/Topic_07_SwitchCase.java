4dccfade5eac592a8f31e209ca050c2164c00d90
minhtn.pegaxy <143593932+minhtn-pegaxy@users.noreply.github.com>
2024-09-17 17:33:47 +0700

    Topic 07 Switch case exercise

--- src/java_basic/Topic_07_SwitchCase.java
+++ src/java_basic/Topic_07_SwitchCase.java
@@ -1,8 +1,41 @@
 package java_basic;
 
+import java.util.Scanner;
+
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
 
 public class Topic_07_SwitchCase {
 	WebDriver driver;
 	String projecPath = System.getProperty("user.dir");
+	Scanner scanner = new Scanner(System.in);
+	
+	@Test
+	public void TC_01 () {
+		int month = scanner.nextInt();
+		
+		switch (month) {
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			System.out.println("This month have 31 days");
+			break;
+		case 2:
+			System.out.println("This month have 28 days");
+			break;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			System.out.println("This monthe have 30 days");
+			break;
+		default:
+			System.out.println("Wrong input");
+		}
+	}
+	
 }
