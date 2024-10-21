c40ca8276a9c09582035eab2bd47ef3fffb71410
minhtn.pegaxy <143593932+minhtn-pegaxy@users.noreply.github.com>
2024-10-21 18:14:15 +0700

    topic 12 String

--- src/java_basic/Topic_12_String.java
+++ src/java_basic/Topic_12_String.java
@@ -1,14 +1,73 @@
 package java_basic;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Topic_12_String {
 	public static void main(String[] agrs) {
-		String s1 = "Cat";
-		String s2 = s1;
-		String s3 = new String("Cat");
+		System.setProperty("webdriver.gecko.driver", ".\\browserDrivers\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
 		
-		System.out.println(s1 == s2);
-		System.out.println(s1 == s3);
-		System.out.println(s2 == s3);
-		System.out.println(s2.equals(s3));
+		// String is immutable, when a new value is created, the system will create a
+		// new string, not replace
+		String schoolName = "Automation Testing";
+		String schoolAddress = "HCM City";
+		String courseName = "AUTOMATION TESTING";
+
+		// length: the character's number of the string
+		System.out.println(schoolName.length());
+
+		// charAt: get the character at selected position in the string
+		System.out.println(schoolName.charAt(1));
+
+		// concat: merge 2 string
+		System.out.println(schoolName.concat(schoolAddress));
+
+		// equals: compare if 2 string is completely equal
+		System.out.println(schoolName.equals(schoolAddress));
+		System.out.println(schoolName.equals("Automation Testing"));
+
+		// equalsIgnoreCase: compare if 2 string is relative equal
+		System.out.println(schoolName.equalsIgnoreCase(courseName));
+
+		// startsWith/ endsWith/ contains
+		System.out.println(schoolAddress.startsWith("H"));
+		System.out.println(schoolName.endsWith("City"));
+		System.out.println(courseName.contains("C"));
+		
+		// index: position of the character/ string in the string
+		System.out.println(schoolName.indexOf("M"));
+		System.out.println(schoolName.indexOf("TOMA"));
+		
+		// substring: split character/ string from string
+		System.out.println(schoolAddress.substring(5));
+		System.out.println(schoolName.substring(3, 7));
+		
+		// split: split text to array based on character or string
+		String result = "Viewin 48 of 132 results";
+		String results[] = result.split(" ");
+		
+		System.out.println(results[1]);
+		
+		// Replace: replace a character or string
+		String productPrice = "$100.00";
+		productPrice.replace("$", "");
+		System.out.println(productPrice);
+		
+		// To sort data
+		float productPriceF = Float.parseFloat(productPrice);
+		System.out.println(productPriceF);
+		
+		productPrice = String.valueOf(productPriceF);
+		System.out.println(productPrice);
+		
+		String osName = System.getProperty("os.name");
+		System.out.println(osName);
+		
+		// handle multiple OS: Mac/ Windows
+		
+		String driverInstanceName = driver.toString();
+		System.out.println(driverInstanceName);
 	}
+
 }
