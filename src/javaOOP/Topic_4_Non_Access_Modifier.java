7406204c3cfc3c8517bdab8bf29dcff625935747
minhtn.pegaxy <143593932+minhtn-pegaxy@users.noreply.github.com>
2024-10-25 13:42:50 +0700

    java OOP topic 4

--- src/javaOOP/Topic_4_Non_Access_Modifier.java
+++ src/javaOOP/Topic_4_Non_Access_Modifier.java
@@ -1,5 +1,26 @@
 package javaOOP;
 
 public class Topic_4_Non_Access_Modifier {
-	 
+	// Static:
+	// - Variable/ Field/ Property
+	// - Method
+	// Can be directly access in a static method
+	// Can be used for all instance/ object
+	// Can be override
+	static String BROWSER = "Chrome";
+
+	// Non-static
+	String serverName = "Testing";
+
+	public static void main(String[] args) {
+		System.out.println(BROWSER);
+
+		BROWSER = "Firefox";
+		System.out.println(BROWSER);
+
+		// Must be init an object to use non-static instance
+		Topic_4_Non_Access_Modifier topic = new Topic_4_Non_Access_Modifier();
+		System.out.println(topic.serverName);
+	}
+
 }
